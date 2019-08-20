d7595d43bede863bd5070346825a3e1b188b95ce
Okafor Chidimma <chidimma.okafor.c@gmail.com>
2019-08-20 14:47:12 +0100

    trying to push file

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+
+import dotenv from 'dotenv';
+dotenv.config();
 const express = require('express');
 const server = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const pg = require('pg');
 pg.connect('postgres://cgoaynxe:Oel5HImw-4ROsgAF0dsWiT4mNuYaHfw3@raja.db.elephantsql.com:5432/cgoaynxe');
 server.listen(PORT, () => console.log(`Server running on ${PORT}`));
